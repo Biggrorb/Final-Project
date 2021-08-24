d0f5d09c2e802a0250a3707ce71d0bc1a9477b35
Biggrorb <87675199+Biggrorb@users.noreply.github.com>
2021-08-23 22:25:17 -0400

    FirstTest

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -1,3 +1,4 @@
+from io import open_code
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -8,24 +9,46 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request,selection):
+  page = request.args.get('page',1,type=int)
+  start = (page - 1) * QUESTIONS_PER_PAGE
+  end = start + QUESTIONS_PER_PAGE
+
+  current_questions = selection[start:end]
+  
+  return current_questions
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
-  
+
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
-
+  CORS(app, resources={r"/*": {'origins': '*'}})
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
-
+  @app.after_request
+  def after_request(response):
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+      response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+      return response
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  @app.route('/categories', methods= ['GET'])
+  def get_categories():
+    selection = Category.query.order_by(Category.id).all()
+    
+    return jsonify({
+      'success':True,
+      'categories': selection,
+    })
+
 
 
   '''
@@ -34,12 +57,34 @@ def create_app(test_config=None):
   including pagination (every 10 questions). 
   This endpoint should return a list of questions, 
   number of total questions, current category, categories. 
-
+  
   TEST: At this point, when you start the application
   you should see questions and categories generated,
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+  @app.route('/questions', methods=['GET'])
+  def get_questions():
+    selection=Question.query.all()
+    questions=paginate_questions(request,selection)
+    categories=Category.query.all()
+    totalquestions=len(selection)
+    formatted_questions=[]
+    for question in questions:
+      one_question = question.format()
+      formatted_questions.append(one_question)
+    formatted_categories=[]
+    for category in categories:
+      one_category = category.format()
+      formatted_categories.append(one_category)
+
+    return jsonify({
+      'success':True,
+      'questions':formatted_questions,
+      'total_questions':totalquestions,
+      'categories':formatted_categories,
+      'current_category':"History"
+    })
 
   '''
   @TODO: 
